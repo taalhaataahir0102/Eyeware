16cf3c1ce0d0b3d54b5ae3808a05f0ab9b03b4d8
taalhaataahir0102 <23100293@lums.edu.pk>
2023-04-08 21:09:06 +0500

    add

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+const { spawn } = require('child_process');
 
 
 const app = express();
@@ -58,6 +59,43 @@ app.get('/review', async (req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
 });
+
+
+
+
+app.get('/add', (req, res) => {
+  // Get the query parameters from the request
+  const num1 = parseInt(3);
+  const num2 = parseInt(4);
+
+  // Spawn a child process that runs the Python script and passes in the arguments
+  const pythonProcess = spawn('python3', ['mymodule.py', num1, num2]);
+
+  // Listen for data from the Python process
+  pythonProcess.stdout.on('data', (data) => {
+    const result = data.toString().trim();
+    console.log(`Result from Python: ${result}`);
+    res.send(`The sum of ${num1} and ${num2} is ${result}`);
+  });
+
+  // Listen for errors from the Python process
+  pythonProcess.stderr.on('data', (data) => {
+    console.error(`Error from Python: ${data}`);
+    res.status(500).send('An error occurred while running the Python script.');
+  });
+
+  // Listen for the Python process to exit
+  pythonProcess.on('close', (code) => {
+    console.log(`Python process exited with code ${code}`);
+  });
+
+  // Listen for errors from the Python process
+  pythonProcess.on('error', (err) => {
+    console.error(`Error executing Python script: ${err}`);
+    res.status(500).send('An error occurred while running the Python script.');
+  });
+});
+
 
 
   
