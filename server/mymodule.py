1c6855f04d116f95130f5194d16d7603123b3c14
taalhaataahir0102 <23100293@lums.edu.pk>
2023-04-08 19:40:49 +0500

    prev

--- server/mymodule.py
+++ server/mymodule.py
@@ -1,15 +1,14 @@
 import sys
 
-if len(sys.argv) != 3:
-    print("Error: Expected two integer arguments")
-    sys.exit(1)
+def add_two_numbers(num1, num2):
+    sum = num1 + num2
+    print(sum)
+    return sum
 
-try:
-    num1 = int(sys.argv[1])
-    num2 = int(sys.argv[2])
-except ValueError:
-    print("Error: Expected integer arguments")
-    sys.exit(1)
-
-result = num1 + num2
-print(result)
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        num1 = int(sys.argv[1])
+        num2 = int(sys.argv[2])
+        add_two_numbers(num1, num2)
+    else:
+        print("Please provide two integers as command line arguments.")
