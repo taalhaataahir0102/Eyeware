e0321ddac64f3c5aeacb30d39a092e31c92814b7
taalhaataahir0102 <23100293@lums.edu.pk>
2023-04-08 19:38:26 +0500

    vercel

--- server/index.js
+++ server/index.js
@@ -60,41 +60,38 @@ app.get('/review', async (req, res) => {
 });
 
 
-app.get('/add', (req, res) => {
-  // Get the query parameters from the request
-  const num1 = parseInt(2);
-  const num2 = parseInt(4);
+// app.get('/add', (req, res) => {
+//   // Get the query parameters from the request
+//   const num1 = parseInt(2);
+//   const num2 = parseInt(4);
 
-  // Spawn a child process that runs the Python script and passes in the arguments
-  const pythonProcess = spawn('python', ['mymodule.py', num1, num2]);
+//   // Spawn a child process that runs the Python script and passes in the arguments
+//   const pythonProcess = spawn('python3', ['mymodule.py', num1, num2]);
 
-  // Listen for data from the Python process
-  pythonProcess.stdout.on('data', (data) => {
-    const result = data.toString().trim();
-    console.log(`Result from Python: ${result}`);
-    res.json(`The sum of ${num1} and ${num2} is ${result}`);
-  });
+//   // Listen for data from the Python process
+//   pythonProcess.stdout.on('data', (data) => {
+//     const result = data.toString().trim();
+//     console.log(`Result from Python: ${result}`);
+//     res.json(`The sum of ${num1} and ${num2} is ${result}`);
+//   });
 
-  // Listen for errors from the Python process
-  pythonProcess.stderr.on('data', (data) => {
-    console.error(`Error from Python: ${data}`);
-    res.status(500).send('An error occurred while running the Python script.');
-  });
+//   // Listen for errors from the Python process
+//   pythonProcess.stderr.on('data', (data) => {
+//     console.error(`Error from Python: ${data}`);
+//     res.status(500).send('An error occurred while running the Python script.');
+//   });
 
-  // Listen for the Python process to exit
-  pythonProcess.on('close', (code) => {
-    console.log(`Python process exited with code ${code}`);
-  });
+//   // Listen for the Python process to exit
+//   pythonProcess.on('close', (code) => {
+//     console.log(`Python process exited with code ${code}`);
+//   });
 
-  // Listen for errors from the Python process
-  pythonProcess.on('error', (err) => {
-    console.error(`Error executing Python script: ${err}`);
-    res.status(500).send('An error occurred while running the Python script.');
-  });
-});
-
-
-
+//   // Listen for errors from the Python process
+//   pythonProcess.on('error', (err) => {
+//     console.error(`Error executing Python script: ${err}`);
+//     res.status(500).send('An error occurred while running the Python script.');
+//   });
+// });
 
 
 
--- server/mymodule.py
+++ server/mymodule.py
@@ -1,14 +1,15 @@
 import sys
 
-def add_two_numbers(num1, num2):
-    sum = num1 + num2
-    print(sum)
-    return sum
+if len(sys.argv) != 3:
+    print("Error: Expected two integer arguments")
+    sys.exit(1)
 
-if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        num1 = int(sys.argv[1])
-        num2 = int(sys.argv[2])
-        add_two_numbers(num1, num2)
-    else:
-        print("Please provide two integers as command line arguments.")
+try:
+    num1 = int(sys.argv[1])
+    num2 = int(sys.argv[2])
+except ValueError:
+    print("Error: Expected integer arguments")
+    sys.exit(1)
+
+result = num1 + num2
+print(result)
